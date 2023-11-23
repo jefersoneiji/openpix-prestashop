85a4afbb6b93b9509325475b8c5a7e391ec7fa9d
codeLearnerrr <jeferson.eiji@icloud.com>
2023-11-23 16:46:52 -0300

    feat: added plugin text translations for portuguese, removed unused templates from module and changed limited_countries and limited_currencies to US and BR

---
+++ translations/br.php
@@ -0,0 +1,27 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{woovi}prestashop>woovi_782b0e97e669dba1c50fa02e2d865b36'] = 'Woovi';
+$_MODULE['<{woovi}prestashop>woovi_1f97ef9258cd1c53442362735e6ecabe'] = 'Módulo da Woovi para pagamentos via Pix';
+$_MODULE['<{woovi}prestashop>woovi_92d4773a9f32fc3333cb8238471cf763'] = 'Para instalar esse módulo é necessário habilitar a extensão cURL';
+$_MODULE['<{woovi}prestashop>woovi_cdbe2f170711683cb6624cf9c509cc99'] = 'Módulo indisponível no seu país';
+$_MODULE['<{woovi}prestashop>woovi_8381013d02d32681a3133497b960373f'] = 'Pagamento Pix via Woovi';
+$_MODULE['<{woovi}prestashop>woovi_6ad64f95bc89b12c2e5c310d84fded7c'] = 'Transações livres de tarifas via Pix';
+$_MODULE['<{woovi}prestashop>woovi_b191593bc602ff910417b3f85bb496ca'] = 'Configuração';
+$_MODULE['<{woovi}prestashop>woovi_a77214d54ec8f3e34e26e03017492f82'] = 'AppID ';
+$_MODULE['<{woovi}prestashop>woovi_3bdda7be8b5c6634144f82a1dd68a86a'] = 'Título da opção de pagamento';
+$_MODULE['<{woovi}prestashop>woovi_432b4ec5b71b7f1b2f2a1c71170492ec'] = 'Descrição da opção de pagamento';
+$_MODULE['<{woovi}prestashop>woovi_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{woovi}prestashop>confirmation_66b914a3d0878daed70f10d165196200'] = 'Ops! Um erro aconteceu. Entre em contato com o vendedor para mais informações.';
+$_MODULE['<{woovi}prestashop>validation_e61b0be95b713b117d27dbd19d75f69a'] = 'Um erro aconteceu durante o processamento do pagamento';
+$_MODULE['<{woovi}prestashop>error_a25c753ee3e4be15ec0daa5a40deb7b8'] = 'Um erro ocorreu';
+$_MODULE['<{woovi}prestashop>confirmation_f303f6ee0c3108351ee9690ba61f5ecb'] = 'Um e-mail será enviado após a confirmação do pagamento.';
+$_MODULE['<{woovi}prestashop>confirmation_b2f40690858b404ed10e62bdf422c704'] = 'Total';
+$_MODULE['<{woovi}prestashop>confirmation_63d5049791d9d79d86e9a108b0a999ca'] = 'Código do Pedido';
+$_MODULE['<{woovi}prestashop>confirmation_19c419a8a4f1cd621853376a930a2e24'] = 'Um e-mail foi enviado com essa informação.';
+$_MODULE['<{woovi}prestashop>confirmation_ca7e41a658753c87973936d7ce2429a8'] = 'Para dúvidas, comentários ou sugestões, por favor entre em contato com';
+$_MODULE['<{woovi}prestashop>confirmation_cd430c2eb4b87fb3b49320bd21af074e'] = 'nosso time de suporte.';
+$_MODULE['<{woovi}prestashop>confirmation_433019aec97a30513f288603a096903e'] = 'QR Code do pagamento pix';
+$_MODULE['<{woovi}prestashop>confirmation_7569ab9b5973795ce8c9fc870d38d8e1'] = 'Seu pedido não foi aceito';
+$_MODULE['<{woovi}prestashop>confirmation_caa4088f1d295cf8ce8e358eb975ab32'] = 'Por favor, tente finalizar o pedido novamente.';
