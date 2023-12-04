87a7994319a10bf62341feaa7c850fd2163462ae
codeLearnerrr <jeferson.eiji@icloud.com>
2023-12-04 11:27:14 -0300

    fix: updated default label text to "Pix by Woovi"

--- woovi.php
+++ woovi.php
@@ -101,7 +101,7 @@ class Woovi extends PaymentModule
 
         Configuration::updateValue('WOOVI_APP_ID_OPENPIX', '');
         Configuration::updateValue('OPENPIX_PUBLIC_KEY_BASE64', 'LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlHZk1BMEdDU3FHU0liM0RRRUJBUVVBQTRHTkFEQ0JpUUtCZ1FDLytOdElranpldnZxRCtJM01NdjNiTFhEdApwdnhCalk0QnNSclNkY2EzcnRBd01jUllZdnhTbmQ3amFnVkxwY3RNaU94UU84aWVVQ0tMU1dIcHNNQWpPL3paCldNS2Jxb0c4TU5waS91M2ZwNnp6MG1jSENPU3FZc1BVVUcxOWJ1VzhiaXM1WloySVpnQk9iV1NwVHZKMGNuajYKSEtCQUE4MkpsbitsR3dTMU13SURBUUFCCi0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=');
-        Configuration::updateValue('WOOVI_LABEL_TITLE', $this->l('Pix payment by Woovi'));
+        Configuration::updateValue('WOOVI_LABEL_TITLE', $this->l('Pix by Woovi'));
         Configuration::updateValue('WOOVI_LABEL_DESCRIPTION', $this->l('Tax free payments using pix'));
 
         return parent::install() &&
